cb6cda09b1a08cdde9bb1c249d83979e8771fdcc
Eren Okka <erenokka@gmail.com>
2018-07-10 17:02:01 +0300

    Add tests for type.hpp

---
+++ test/type.cpp
@@ -0,0 +1,34 @@
+#include <cassert>
+#include <cctype>
+#include <iostream>
+#include <limits>
+
+#include "../nstd/type.hpp"
+
+namespace {
+
+void test_chars() {
+  for (int c = 0; c <= std::numeric_limits<char>::max(); ++c) {
+    assert(nstd::isalnum(c) == (std::isalnum(c) != 0));
+    assert(nstd::isalpha(c) == (std::isalpha(c) != 0));
+    assert(nstd::islower(c) == (std::islower(c) != 0));
+    assert(nstd::isupper(c) == (std::isupper(c) != 0));
+    assert(nstd::isdigit(c) == (std::isdigit(c) != 0));
+    assert(nstd::isxdigit(c) == (std::isxdigit(c) != 0));
+    assert(nstd::iscntrl(c) == (std::iscntrl(c) != 0));
+    assert(nstd::isgraph(c) == (std::isgraph(c) != 0));
+    assert(nstd::isspace(c) == (std::isspace(c) != 0));
+    assert(nstd::isblank(c) == (std::isblank(c) != 0));
+    assert(nstd::isprint(c) == (std::isprint(c) != 0));
+    assert(nstd::ispunct(c) == (std::ispunct(c) != 0));
+    assert(nstd::tolower(c) == std::tolower(c));
+    assert(nstd::toupper(c) == std::toupper(c));
+  }
+}
+
+}  // namespace
+
+int main() {
+  test_chars();
+  return 0;
+}
