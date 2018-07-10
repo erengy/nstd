9fff0f702beac18242a5774ba5c2c4a0983df1b5
Eren Okka <erenokka@gmail.com>
2018-07-10 19:22:23 +0300

    Add tests for comparable.hpp

--- nstd/comparable.hpp
+++ nstd/comparable.hpp
@@ -26,7 +26,7 @@ SOFTWARE.
 
 namespace nstd {
 
-template<class T>
+template <typename T>
 class comparable {
 public:
   virtual int compare(const T& rhs) const = 0;
@@ -50,5 +50,10 @@ public:
     return compare(rhs) >= 0;
   }
 };
+
+template <typename T>
+int compare(const T& lhs, const T& rhs) {
+  return (lhs != rhs) ? (lhs < rhs ? -1 : 1) : 0;
+}
 
 }  // namespace nstd
---
+++ test/comparable.cpp
@@ -0,0 +1,35 @@
+#include <cassert>
+#include <iostream>
+
+#include "../nstd/comparable.hpp"
+
+namespace {
+
+void test_compare_int() {
+  class Integer : public nstd::comparable<Integer> {
+  public:
+    constexpr Integer(int value) : value_{value} {}
+    constexpr int value() const { return value_; }
+    int compare(const Integer& rhs) const override {
+      return nstd::compare(value(), rhs.value());
+    }
+  private:
+    int value_ = 0;
+  };
+
+  assert(Integer{0} == Integer{0});
+  assert(Integer{0} != Integer{1});
+  assert(Integer{0} <  Integer{1});
+  assert(Integer{1} >  Integer{0});
+  assert(Integer{0} <= Integer{1});
+  assert(Integer{1} <= Integer{1});
+  assert(Integer{1} >= Integer{0});
+  assert(Integer{1} >= Integer{1});
+}
+
+}  // namespace
+
+int main() {
+  test_compare_int();
+  return 0;
+}
